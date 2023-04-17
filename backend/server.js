e1ef04f34092282148d96939f1f9c8e38c113e2c
ethanwhitcher <ewhitcher123@gmail.com>
2023-04-17 00:46:58 -0400

    setup backend dependencies and server

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+    res.end('This is my response');
+})
+
+app.get('/api', (req, res) => {
+    res.send('This is the response');
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
