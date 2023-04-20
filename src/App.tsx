79e283e8b2fda033378adec87074cb7aa6cfe0bf
Jwiggiff <friedman.josh03@gmail.com>
2023-04-20 16:32:52 -0400

    Setup Frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      
-    </div>
-  )
+    return <div className="App"></div>;
 }
-
-export default App
