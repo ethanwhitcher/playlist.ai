7297f382dce4e8197664271dbc14f0428726d1b8
laurenlidhar <98188715+laurenlidhar@users.noreply.github.com>
2023-05-07 15:34:07 -0700

    app landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
+import Button from "./_components/base/Button";
+import Landing from "./_components/Landing";
+import AddSong from "./_components/AddSong";
 
 export default function App() {
     const [count, setCount] = useState(0);
 
-    return <div className="App"></div>;
+    const onClickHandler = () => {
+        props.onClick;
+    };
+
+    return (
+        <div className="App">
+          <Landing />
+        </div>
+    );
 }
