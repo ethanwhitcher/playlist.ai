15a06d8b9c5bc833a2a329adcf4972c0fb3c591c
Jwiggiff <friedman.josh03@gmail.com>
2023-05-09 23:13:10 -0400

    Format Landing and remove share btn

--- src/_components/Landing.tsx
+++ src/_components/Landing.tsx
@@ -6,13 +6,8 @@ type LandingProps = {
 };
 
 export default function Landing({ onClick }: LandingProps) {
-
     return (
         <div className="landing">
-            <div className="sharebtn">
-                <Button variant={"primary"} onClick={onClick} icon={"shareIcon"} title={"Share"} />
-            </div>
-            
             <div className="container">
                 <AddSong />
                 <AddSong />
@@ -20,10 +15,12 @@ export default function Landing({ onClick }: LandingProps) {
             </div>
 
             <div className="genbtn">
-                <Button variant={"primary"} onClick={onClick} title={"GENERATE"} />
+                <Button
+                    variant={"primary"}
+                    onClick={onClick}
+                    title={"GENERATE"}
+                />
             </div>
-            
-            
         </div>
     );
 }
