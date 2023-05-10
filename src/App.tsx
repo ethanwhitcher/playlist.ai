28b4aa743cdd22ed006bf549bd0665f91ad17c9a
Jwiggiff <friedman.josh03@gmail.com>
2023-05-09 23:29:26 -0400

    main page layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,55 +3,58 @@ import Search from "./_components/search/Search";
 import GeneratedPlaylist from "./_components/GeneratedPlaylist";
 import { Playlist } from "./utils/types";
 import Header from "./_components/Header";
+import Landing from "./_components/Landing";
 
 export default function App() {
-    const [playlist, setPlaylist] = useState<Playlist>({
-        title: "Josh's Playlist",
-        image: "https://via.placeholder.com/200",
-        duration: 10650600,
-        songs: [
-            {
-                title: "Song",
-                artist: "john",
-                album: {
-                    title: "Albuuum",
-                    image: "https://via.placeholder.com/100",
-                },
-                duration: 1000 * 60 * 3.25,
-            },
-            {
-                title: "Song",
-                artist: "john",
-                album: {
-                    title: "Albuuum",
-                    image: "https://via.placeholder.com/100",
-                },
-                duration: 1000 * 60 * 3.25,
-            },
-            {
-                title: "Song",
-                artist: "john",
-                album: {
-                    title: "Albuuum",
-                    image: "https://via.placeholder.com/100",
-                },
-                duration: 1000 * 60 * 3.25,
-            },
-            {
-                title: "Song",
-                artist: "john",
-                album: {
-                    title: "Albuuum",
-                    image: "https://via.placeholder.com/100",
-                },
-                duration: 1000 * 60 * 3.25,
-            },
-        ],
-    });
+    const [playlist, setPlaylist] = useState<Playlist | null>(null);
+    // const [playlist, setPlaylist] = useState<Playlist>({
+    //     title: "Josh's Playlist",
+    //     image: "https://via.placeholder.com/200",
+    //     duration: 10650600,
+    //     songs: [
+    //         {
+    //             title: "Song",
+    //             artist: "john",
+    //             album: {
+    //                 title: "Albuuum",
+    //                 image: "https://via.placeholder.com/100",
+    //             },
+    //             duration: 1000 * 60 * 3.25,
+    //         },
+    //         {
+    //             title: "Song",
+    //             artist: "john",
+    //             album: {
+    //                 title: "Albuuum",
+    //                 image: "https://via.placeholder.com/100",
+    //             },
+    //             duration: 1000 * 60 * 3.25,
+    //         },
+    //         {
+    //             title: "Song",
+    //             artist: "john",
+    //             album: {
+    //                 title: "Albuuum",
+    //                 image: "https://via.placeholder.com/100",
+    //             },
+    //             duration: 1000 * 60 * 3.25,
+    //         },
+    //         {
+    //             title: "Song",
+    //             artist: "john",
+    //             album: {
+    //                 title: "Albuuum",
+    //                 image: "https://via.placeholder.com/100",
+    //             },
+    //             duration: 1000 * 60 * 3.25,
+    //         },
+    //     ],
+    // });
 
     return (
         <>
             <Header />
+            <Landing onClick={() => {}} />
             {playlist && <GeneratedPlaylist playlist={playlist} />}
         </>
     );
