5e2a9760653e8672ab81e4bae60853e261cbb625
Jwiggiff <friedman.josh03@gmail.com>
2023-05-21 23:32:51 -0400

    add disabled button

--- src/_components/base/Button.tsx
+++ src/_components/base/Button.tsx
@@ -3,11 +3,21 @@ type ButtonProps = {
     onClick: () => void;
     icon?: string;
     title: string;
+    disabled?: boolean;
 };
 
-export default function Button({ variant, onClick, icon, title }: ButtonProps) {
+export default function Button({
+    variant,
+    onClick,
+    icon,
+    title,
+    disabled,
+}: ButtonProps) {
     return (
-        <button className={`button ${variant}`} onClick={onClick}>
+        <button
+            className={`button ${variant} ${disabled ? "disabled" : ""}`}
+            onClick={onClick}
+        >
             {icon && <img src={icon}></img>}
             {title}
         </button>
