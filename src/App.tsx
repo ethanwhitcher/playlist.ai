66bb750454caa7be85ab96f48e6bca46f52bf239
Jwiggiff <friedman.josh03@gmail.com>
2023-05-22 00:33:03 -0400

    Add loader

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GeneratedPlaylist from "./_components/GeneratedPlaylist";
 import { Playlist, Song } from "./utils/types";
 import Header from "./_components/Header";
 import Landing from "./_components/Landing";
+import Loading from "./_components/Loading";
 
 export const SelectedSongs = createContext<[Song[], (songs: Song[]) => void]>([
     [],
@@ -13,8 +14,11 @@ export const SelectedSongs = createContext<[Song[], (songs: Song[]) => void]>([
 export default function App() {
     const selectedSongsState = useState<Song[]>([]);
     const [playlist, setPlaylist] = useState<Playlist | null>(null);
+    const [loading, setLoading] = useState(false);
 
     const generate = async () => {
+        setLoading(true);
+
         const songs = selectedSongsState[0].map((song) => ({
             title: song.title,
             artist: song.artist,
@@ -37,10 +41,12 @@ export default function App() {
                 songs: res,
             }));
         setPlaylist(pl);
+        setLoading(false);
     };
 
     return (
         <SelectedSongs.Provider value={selectedSongsState}>
+            {loading && <Loading />}
             <Header />
             <Landing onClick={generate} />
             {playlist && <GeneratedPlaylist playlist={playlist} />}
